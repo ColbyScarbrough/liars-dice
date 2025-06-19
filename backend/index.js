54c9840cbaf07684ca0c61f0e24bfa48c78fbad1
cs18472 <CWS18472@gmail.com>
2025-06-19 17:33:28 -0400

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }
+});
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 3001;
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Liars Dice Backend Running');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
